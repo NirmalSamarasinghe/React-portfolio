20cba9b9a2648b4f30f3e7b61676895e5af79113
Nipun78 <nipunnirmal67@gmail.com>
2024-08-05 23:48:39 +0530

    done the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+
 
 function App() {
   return (
-    <div className='overflow-x-hidden text-neutral-300'>
-      <div class="flixed top-0 -z-10 h-full w-full">
+    <div className='overflow-x-hidden text-neutral-300 '>
+      <div class="flixed top-0 -z-10 h-full w-full"></div>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),
       rgba(255,255,255,0))]"></div>
 
-      <div className='container mx-auto px-8'/>
-      </div>
+      <div className='container mx-auto px-8'>
       <NavBar/>
-    </div>
+      <Hero/>
+      </div>
+      </div>
   )
 }
 
